5476bfdd473f0533e9894a584967c588e0349e37
mamun-swe <mamun.swe.277@gmail.com>
2023-07-18 22:47:56 +0600

    props added

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
-export const Navbar: React.FC = (): JSX.Element => {
+type PropsTypes = {
+  title?: string;
+};
+
+export const Navbar: React.FC<PropsTypes> = (
+  props: PropsTypes
+): JSX.Element => {
   return (
     <div className="p-3 border-b bg-white">
-      <p className="text-lg font-medium text-gray-500">Navbar</p>
+      <p className="text-lg font-medium text-gray-500">
+        {props.title || "Navbar"}
+      </p>
     </div>
   );
 };
